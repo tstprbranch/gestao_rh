442015bd7a4c54e60c53f19705d83a28c12db2fa
matheusbriotto <matheusbriotto@gmail.com>
2020-03-18 12:07:45 -0300

    Created ForeignKey and PrimaryKey

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
