6cc874918284c20674a069e724e411c9843b8f56
matheusbriotto <matheusbriotto@gmail.com>
2020-03-20 10:56:11 -0300

    STATIC_ROOT ADD

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -134,6 +134,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
