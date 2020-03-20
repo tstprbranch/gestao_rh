356fb1717ea14a6642e0601f13753977090e9eb0
matheusbriotto <matheusbriotto@gmail.com>
2020-03-20 09:01:41 -0300

    Register hour

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,4 +1,7 @@
+from django.urls import reverse_lazy
+
 from .models import RegistroHoraExtra
+from .forms import RegistroHoraExtraForm
 from django.views.generic import (
     ListView,
     UpdateView,
@@ -13,3 +16,22 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
+class HoraExtraNovo(CreateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraNovo, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
