95d29c2a896f49c2692d7064f5bf029613100dfb
matheusbriotto <matheusbriotto@gmail.com>
2020-03-24 15:44:24 -0300

    bug fix in the functional and department tables

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -30,7 +30,7 @@ class FuncionarioNovo(CreateView):
 
     def form_valid(self, form):
         funcionario = form.save(commit=False)
-        username = funcionario.nome.split(' ') [0] + funcionario.nome.split(' ') [1]
+        username = funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1]
         funcionario.empresa = self.request.user.funcionario.empresa
         funcionario.user = User.objects.create(username=username)
         funcionario.save()
